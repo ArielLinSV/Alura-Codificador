bd19497eeacfa20914d13beee0e052859f06fa43
Ariel Linares <lmorchest@gmail.com>
2023-05-26 17:43:03 -0600

    v1 del repositorio del codificador

---
+++ HTML code/JScode.js
@@ -0,0 +1,46 @@
+const encriptar = document.getElementById("encryptbutton");
+const desencriptar = document.getElementById("desencryptbutton");
+const copiar = document.getElementById("copybutton");
+const data = document.getElementById("inputtext");
+const resultadoMensaje = document.querySelector(".resultado_mensaje");
+const resultado = document.getElementById("result");
+const noEncontrado = document.querySelector(".no_encontrado");
+const encontrado = document.querySelector(".encontrado");
+
+encriptar.addEventListener("click", () => {
+    resultado.value = encriptarMensaje(data.value);
+    noEncontrado.style.display = "none";
+    encontrado.style.display = "block";
+});
+
+desencriptar.addEventListener("click", () => {
+    resultado.value = desencriptarMensaje(data.value);
+    noEncontrado.style.display = "none";
+    encontrado.style.display = "block";
+});
+
+copiar.addEventListener("click", () => {
+    copiarMensaje();
+});
+
+function encriptarMensaje(value) {
+    return value
+        .replaceAll("e","enter")
+        .replaceAll("i","imes")
+        .replaceAll("o","ober")
+        .replaceAll("a","ai")
+        .replaceAll("u","ufat");
+}
+
+function desencriptarMensaje(value) {
+    return value
+        .replaceAll("enter","e")
+        .replaceAll("imes","i")
+        .replaceAll("ober","o")
+        .replaceAll("ai","a")
+        .replaceAll("ufat","u");
+}
+
+function copiarMensaje() {
+    navigator.clipboard.writeText(resultado.value);
+}
